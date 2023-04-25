6f377ebc29e14b78ea39120b8485fedc63b6f56b
jfouret <julien.fouret.fr@gmail.com>
2023-04-25 17:58:02 +0000

    update parsing and docs

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+
+# Demuxator
+
+## What is it ?
+
+A small C++ program to demultiplex paired reads.
+
+```
+$ ./demuxator -h
+Usage: ./demuxator [-e|--expected-barcodes expected_barcodes.txt] [-t|--threshold threshold] [--subset-size subset_size] [--bc-start bc_start] [--bc-length bc_length] [--remove-barcode] read1.fastq.gz read2.fastq.gz
+  -e, --expected-barcodes: Path to the expected barcodes file (default: none)
+  -t, --threshold: Threshold for selecting barcodes based on their relative occurrence in the subset (default: 0.01)
+  --subset-size: Subset size for selecting barcodes (default: 10000)
+  --bc-start: Start position of barcodes in read 2 (default: 10)
+  --bc-length: Length of barcodes in read 2 (default: 12)
+  --remove-barcode: Remove barcode from output reads (default: false)
+  -h, --help: Show this help message
+Output files are written in the current directory using the barcode sequence and prefix name.
+```
+
+## Build
+
+- Simply use `make`.
+- `make test` will run tests
+- `make coverage_report` will report the coverage of the tests
+
+## Dependancies
+
+### To build the program
+
+- zlib
+- c++ Compiler
+- c++ standard libraries
+
+### To run the tests
+
+On debian 11:
+
+```
 sudo apt-get install gcovr lcov libgtest-dev
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <cassert>
@@ -11,23 +10,27 @@
 #include "select_barcodes_above_threshold.h"
 #include "demuxator.h"
 
-constexpr int SUBSET_SIZE = 10000;
-constexpr int BC_START = 10;
-constexpr int BC_LENGTH = 12;
-
 int main(int argc, char *argv[]) {
     int opt;
     std::string expected_barcodes_path;
     double threshold = 0.01;
-    
+    int subset_size = 10000;
+    int bc_start = 10;
+    int bc_length = 12;
+    bool remove_barcode = false;
+
     static struct option long_options[] = {
         {"expected-barcodes", required_argument, 0, 'e'},
         {"threshold", required_argument, 0, 't'},
+        {"subset-size", optional_argument, 0, 's'},
+        {"bc-start", optional_argument, 0, 'b'},
+        {"bc-length", optional_argument, 0, 'l'},
+        {"help", no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
 
     int long_index = 0;
-    while ((opt = getopt_long(argc, argv, "e:t:", long_options, &long_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "e:t:s:b:l:h", long_options, &long_index)) != -1) {
         switch (opt) {
             case 'e':
                 expected_barcodes_path = optarg;
@@ -35,14 +38,35 @@ int main(int argc, char *argv[]) {
             case 't':
                 threshold = std::stod(optarg);
                 break;
+            case 's':
+                subset_size = std::stoi(optarg);
+                break;
+            case 'b':
+                bc_start = std::stoi(optarg);
+                break;
+            case 'l':
+                bc_length = std::stoi(optarg);
+                break;
+            case 'r':
+                remove_barcode = true;
+                break;
+            case 'h':
             default:
-                std::cerr << "Usage: " << argv[0] << " [-e|--expected-barcodes expected_barcodes.txt] [-t|--threshold threshold] read1.fastq.gz read2.fastq.gz" << std::endl;
-                return 1;
+                std::cerr << "Usage: " << argv[0] << " [-e|--expected-barcodes expected_barcodes.txt] [-t|--threshold threshold] [--subset-size subset_size] [--bc-start bc_start] [--bc-length bc_length] [--remove-barcode] read1.fastq.gz read2.fastq.gz" << std::endl;
+                std::cerr << "  -e, --expected-barcodes: Path to the expected barcodes file (default: none)" << std::endl;
+                std::cerr << "  -t, --threshold: Threshold for selecting barcodes based on their relative occurrence in the subset (default: 0.01)" << std::endl;
+                std::cerr << "  --subset-size: Subset size for selecting barcodes (default: 10000)" << std::endl;
+                std::cerr << "  --bc-start: Start position of barcodes in read 2 (default: 10)" << std::endl;
+                std::cerr << "  --bc-length: Length of barcodes in read 2 (default: 12)" << std::endl;
+                std::cerr << "  --remove-barcode: Remove barcode from output reads (default: false)" << std::endl;
+                std::cerr << "  -h, --help: Show this help message" << std::endl;
+                std::cerr << "Output files are written in the current directory using the barcode sequence and prefix name." << std::endl;
+                return (opt == 'h') ? 0 : 1;
         }
     }
 
     if (optind + 2 > argc) {
-        std::cerr << "Usage: " << argv[0] << " [-e|--expected-barcodes expected_barcodes.txt] [-t|--threshold threshold] read1.fastq.gz read2.fastq.gz" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [-e|--expected-barcodes expected_barcodes.txt] [-t|--threshold threshold] [--subset-size subset_size] [--bc-start bc_start] [--bc-length bc_length] [--remove-barcode] read1.fastq.gz read2.fastq.gz" << std::endl;
         return 1;
     }
 
@@ -60,8 +84,8 @@ int main(int argc, char *argv[]) {
     if (!expected_barcodes_path.empty()) {
         allowed_barcodes = load_expected_barcodes(expected_barcodes_path);
     }
-    
-    std::unordered_set<std::string> barcodes_above_threshold = select_barcodes_above_threshold(read2_file, BC_START, BC_LENGTH, threshold, SUBSET_SIZE);
+
+    std::unordered_set<std::string> barcodes_above_threshold = select_barcodes_above_threshold(read2_file, bc_start, bc_length, threshold, subset_size);
     // If both expected barcodes and barcodes above threshold are provided, take the union of both sets
     if (!allowed_barcodes.empty()) {
         for (const auto &barcode : barcodes_above_threshold) {
@@ -71,8 +95,8 @@ int main(int argc, char *argv[]) {
         allowed_barcodes = barcodes_above_threshold;
     }
 
-    demuxator(read1_file, read2_file, allowed_barcodes, BC_START, BC_LENGTH, true);
+    demuxator(read1_file, read2_file, allowed_barcodes, bc_start, bc_length, remove_barcode);
 
     return 0;
-}
 
+}
